73d0aa38bbcbb16e3cd71e8f123d21f2cdee7991
Mohd Abdul Mannan <ma24042000@gmail.com>
2024-04-08 19:19:20 +0530

    submit

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  let ip = req.ip;
+  let language =  req.headers['accept-language'];
+  let software = req.headers['user-agent'];
+  console.log(ip);
+  res.json({ ipaddress: ip, language: language, software: software});
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
